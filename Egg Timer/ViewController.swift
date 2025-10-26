ad5e7e38868e0ea80f9ab710272a0fe326452f37
Jeevan Jaison <jeevanjaisonjmj@gmail.com>
2025-10-26 23:47:07 +0530

    day 26 oct

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -1,19 +1,42 @@
 //
 //  ViewController.swift
-//  Egg Timer
+//  EggTimer
 //
-//  Created by Jeevan Jaison on 24/10/25.
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let eggTimes=["Soft":3,"Medium":4,"Hard":7]
+    var secondsRemaining=0
+    
+    var timer=Timer()
+    
+    @IBOutlet weak var topLabel: UILabel!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        topLabel.text="\(sender.currentTitle!) Egg selected"
+        timer.invalidate()
+        
+        var eggType=sender.currentTitle!
+        print(eggTimes[eggType]!)
+        
+        secondsRemaining=eggTimes[eggType]!
+        
+        timer=Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
-
-
+    
+    @objc func updateTimer(){
+        if secondsRemaining>0{
+            print("\(secondsRemaining) Seconds")
+            secondsRemaining-=1
+        }
+        else{
+            timer.invalidate()
+            topLabel.text="Egg is Done!"
+        }
+    }
+    
 }
-
